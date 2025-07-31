fcd2a4f6eedb5afc7c02a0648e02b84d1280c8d0
Nikolaj Jensen Ulrik <nikolaj@njulrik.dk>
2025-07-31 18:02:45 +0200

    forcing spaces instead of tabs (to be undone in go?)

--- init.lua
+++ init.lua
@@ -158,6 +158,10 @@ vim.o.inccommand = 'split'
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
